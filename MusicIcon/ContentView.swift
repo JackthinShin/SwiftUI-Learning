dce1e80d32969eb115aa6cdf6bf28774682d95b3
JackthinShin <jackthin@foxmail.com>
2025-09-29 17:02:31 +0800

    Innitial Commit The First Homework of AiTe Club iOS Group Demonstrating a Red Music Icon Accompanied With Text 'Music'

--- MusicIcon/ContentView.swift
+++ MusicIcon/ContentView.swift
@@ -4,16 +4,23 @@
 //
 //  Created by Jackthin Shin on 2025/9/29.
 //
+//  The First Application of Mine
+//  The First Homework of AiTe Club iOS Group
+//  Demonstrating a Red Music Icon
+//  Accompanied With Text 'Music'
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "music.quarternote.3")
+                .font(.system(size: 50))
+                .foregroundStyle(.red)
+            .padding()
+            Text("Music")
+                .bold()
+                .font(.system(size: 50))
         }
         .padding()
     }
