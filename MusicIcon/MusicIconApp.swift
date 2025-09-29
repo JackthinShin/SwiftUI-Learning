dc075044ac68024c46220dcf07860e0f4264721b
JackthinShin <jackthin@foxmail.com>
2025-09-29 09:00:05 +0800

    Initial Commit

---
+++ MusicIcon/MusicIconApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicIconApp.swift
+//  MusicIcon
+//
+//  Created by Jackthin Shin on 2025/9/29.
+//
+
+import SwiftUI
+
+@main
+struct MusicIconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
