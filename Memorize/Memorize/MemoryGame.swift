a25d338fc1b053132df89f8aa8546a10f248cb56
Jackthin <jackthin@foxmail.com>
2025-12-07 10:55:01 +0800

    Sudoku Game

--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -9,8 +9,11 @@
 import Foundation
 
 struct MemoryGame<CardContent> {
+    // cards是Card数据类型的数组
     private(set) var cards: Array<Card>
     
+    // numberOfPairsOfCards 牌的对数
+    // cardContentFactory 是一个函数，从int映射到CardContent类型
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent){
         cards = Array<Card>()
         for pairIndex in 0..<max(2, numberOfPairsOfCards) {
@@ -24,6 +27,7 @@ struct MemoryGame<CardContent> {
         
     }
     
+    // mutating允许在内部修改结构体或枚举的属性值
     mutating func shuffle() {
         cards.shuffle()
     }
